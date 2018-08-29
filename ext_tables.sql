29b7ce512008e4d724e1c1e770d80759651ba930
Michael Rosenberger <michael.rosenberger@uni-hohenheim.de>
2018-08-29 09:02:16 +0200

    Unifies indentation in ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,10 +2,10 @@
 # Table structure for table 'tx_swiper_domain_model_slide'
 #
 CREATE TABLE tx_swiper_domain_model_slide (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) DEFAULT '0' NOT NULL,
 
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
   crdate int(11) unsigned DEFAULT '0' NOT NULL,
   cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
   deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
@@ -28,10 +28,10 @@ CREATE TABLE tx_swiper_domain_model_slide (
   l10n_parent int(11) DEFAULT '0' NOT NULL,
   l10n_diffsource mediumblob,
 
-	image varchar(255) DEFAULT '' NOT NULL,
-	content text NOT NULL,
+  image varchar(255) DEFAULT '' NOT NULL,
+  content text NOT NULL,
 
-	PRIMARY KEY (uid),
+  PRIMARY KEY (uid),
   KEY parent (pid),
   KEY t3ver_oid (t3ver_oid,t3ver_wsid),
   KEY language (l10n_parent,sys_language_uid)
